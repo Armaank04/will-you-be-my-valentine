ec0493f4cf44db7e9162e60e99b7d7eeec345f5f
Armaank04 <armaank2004@gmail.com>
2026-01-28 19:47:28 -0500

    config.js

--- config.js
+++ config.js
@@ -76,9 +76,9 @@ const CONFIG = {
     // Background Music (Optional)
     // Add your own music URL after getting proper licenses
     music: {
-        enabled: true,                     // Music feature is enabled
+        enabled: false,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dwegxwedn/video/upload/v1769646323/Stevie_Wonder_-_My_Cherie_Amour_4_r3w07x.mp3", // Music streaming URL
+        musicUrl: "", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
